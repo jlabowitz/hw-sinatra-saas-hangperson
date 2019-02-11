d1cce978832fcaf72d3b56b950eb7151df513615
Jacob Labowitz <jlabowitz@berkeley.edu>
2019-02-11 22:58:08 +0000

    added check for not allowing going to /win or /lose directly

--- app.rb
+++ app.rb
@@ -70,11 +70,17 @@ class HangpersonApp < Sinatra::Base
   
   get '/win' do
     ### YOUR CODE HERE ###
+    if @game.check_win_or_lose != :win
+      redirect '/show'
+    end
     erb :win # You may change/remove this line
   end
   
   get '/lose' do
     ### YOUR CODE HERE ###
+    if @game.check_win_or_lose != :lose
+      redirect '/show'
+    end
     erb :lose # You may change/remove this line
   end
   
