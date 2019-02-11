f654a817de30643e6408d1e4c2d71bb08164585c
Jacob Labowitz <jlabowitz@berkeley.edu>
2019-02-11 21:34:25 +0000

    /guess functionality

--- app.rb
+++ app.rb
@@ -40,6 +40,14 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    begin
+      valid = @game.guess(letter)
+      if !valid
+        flash[:message] = "You have already used that letter."
+      end
+    rescue
+      flash[:message] = "Invalid guess."
+    end
     redirect '/show'
   end
   
